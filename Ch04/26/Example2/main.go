64a044ecce602ae64a9f1792cf67dd440025a704
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2020-12-06 02:18:09 +0200

    Complete Demo code example of Chapter 04/Video[Demo: Working with Closed Channels]

---
+++ Ch04/26/Example2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	// create wait group and add how much to wait
+	waitGroup := &sync.WaitGroup{}
+	waitGroup.Add(2)
+
+	// create channel with integer messages
+	channel := make(chan int)
+
+	// create first Goroutine to will include Print received message from channel
+	go func(ch chan int, wg *sync.WaitGroup) {
+		fmt.Println(<- ch)
+		// close channel after receive message from channel
+		close(ch)
+		// try to receive new message from close message will Zero-value
+		// Zero-value is default value for channel's type, in this case channel for integer messages so zero-value is zero
+		fmt.Println(<- ch)
+		wg.Done()
+	} (channel, waitGroup)
+
+	// create second Goroutine to will include send integer message on channel
+	go func(ch chan int, wg *sync.WaitGroup) {
+		ch <- 42
+		wg.Done()
+	} (channel, waitGroup)
+
+	// finally set waitGroup to wait
+	waitGroup.Wait()
+}
